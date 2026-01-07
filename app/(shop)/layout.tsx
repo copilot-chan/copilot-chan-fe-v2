9e241f83190faf44b9e67f71257b38979ccae37e
Vucongchien <vucongchien204@gmail.com>
2026-01-07 15:20:14 +0700

    feat(shop): update shop layout

--- app/(shop)/layout.tsx
+++ app/(shop)/layout.tsx
@@ -7,17 +7,15 @@ import { ReactNode } from 'react';
 import { Toaster } from 'sonner';
 
 export default function ShopLayout({ children }: { children: ReactNode }) {
-  return (
-    <div className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white min-h-screen">
-      <EcommerceApiProvider>
-        <CartProvider>
-          <Navbar />
-          <main>
-            {children}
-          </main>
-          <Toaster position="bottom-right" />
-        </CartProvider>
-      </EcommerceApiProvider>
-    </div>
-  );
+    return (
+        <div className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white min-h-screen">
+                <EcommerceApiProvider>
+                    <CartProvider>
+                        <Navbar />
+                        <main>{children}</main>
+                        <Toaster position="bottom-right" />
+                    </CartProvider>
+                </EcommerceApiProvider>
+        </div>
+    );
 }
