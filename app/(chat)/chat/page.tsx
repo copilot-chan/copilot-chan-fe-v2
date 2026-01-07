8199fcbe1de2ae4339c161b35e811357b52414f9
Vucongchien <vucongchien204@gmail.com>
2026-01-07 15:20:19 +0700

    feat(chat): add chat page

---
+++ app/(chat)/chat/page.tsx
@@ -0,0 +1,12 @@
+import { ChatInterface } from "@/components/chat/ChatInterface";
+import RegisterMCPServer from "@/components/chat/mcp/SetMCPServer";
+import { CopilotActionRender } from "@/components/chat/actions/copilot-actions";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <CopilotActionRender />
+      <ChatInterface />
+    </main>
+  );
+}
